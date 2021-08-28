03a8621b9d841d0caf2cc9759364879345865360
SJRXD <rohr.olasz@gmail.com>
2021-08-28 15:41:19 -0700

    initial commit

---
+++ Develop/server.js
@@ -0,0 +1,17 @@
+// REQUIRE //
+
+const { v4: uuidv4 } = require("uuid");
+const express = require("express");
+const router = require("express").Router();
+
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+const routes1 = require("./routes/index.js");
+const db = require("./db/db.json");
+
+const PORT = process.env.PORT || 3000;
+
+// app.use
