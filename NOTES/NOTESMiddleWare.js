238d68b5ef2bf28d621c0ac18d16f3a8b3f47589
SJRXD <rohr.olasz@gmail.com>
2021-08-29 19:29:45 -0700

    updating notes

--- NOTES/NOTESMiddleWare.js
+++ NOTES/NOTESMiddleWare.js
@@ -18,43 +18,44 @@
 
 // GLOBAL MIDDLEWARE //
 
-app.use(logger);
+//// app.use(logger);
 // run before other requests
 // middleware runs in order you define it
 // when creating global actions, place at top so they happen before different controller actions
 
 // EXAMPLES //
 
-app.get("/", (req, res) => {
-    res.send("Intro Page")
-    console.log("Intro Page")
-})
+//// app.get("/", (req, res) => {
+//     res.send("Intro Page")
+//     console.log("Intro Page")
+// })
 // accepts path
 // accepts list of different middlware
 
-app.get("/users", auth, (req, res) => {
-    res.send("Note Page")
-    console.log("Note Page")
-    console.log("user is admin = ${req.admin}.")
-})
+//// app.get("/users", auth, (req, res) => {
+//     res.send("Note Page")
+//     console.log("Note Page")
+//     console.log("user is admin = ${req.admin}.")
+// })
 // added auth function
 
-function logger (request, response, next) {
-    console.log("log")
-    console.log(req.originalUrl)
-    next() // next piece of middleware will run
-}
+//// function logger (request, response, next) {
+//     console.log("log")
+//     console.log(req.originalUrl)
+//     next() // next piece of middleware will run
+// }
 
 // MIDDLEWARE SPECIFIC TO SINGLE ACTION //
 
-function auth (request, response, next) {
-    if (req.query.admin === "true") {
-        req.admin = true
-    next()
-    } else { 
-    res.send("NO AUTH")
-    }
-}
+//// function auth (request, response, next) {
+//     if (req.query.admin === "true") {
+//         req.admin = true
+//     next()
+//     } else { 
+//     res.send("NO AUTH")
+//     }
+// }
+
 // check to see if user is authenticated
 // should fail this auth check cuz we didn't pass ADMIN IS TRUE
 // but if localhost:3000/users?admin=true; then true
@@ -62,7 +63,7 @@ function auth (request, response, next) {
 // inside middleware, access request and response so we can exit out OR
 // so we can auth people by checking query parameters
 
-app.listen(3000);
+//// app.listen(3000);
 
 // next does NOT WORK THE SAME AS RETURNING FROM A FUNCTION
 // cannot set headers after they're sent to client
