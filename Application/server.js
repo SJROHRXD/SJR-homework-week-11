fd411b6e37186a2f53043568af0dc6b1abeb5d76
SJRXD <rohr.olasz@gmail.com>
2021-08-30 00:43:54 -0700

    updated routes, server, and js saveData file to actually work thank you god

--- Application/server.js
+++ Application/server.js
@@ -1,104 +1,29 @@
 // REQUIRE 🍓 //
-const moment = require("moment");
-const { v4: uuidv4 } = require("uuid");
 const express = require("express");
-const router = require("express").Router();
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+const moment = require("moment");
 
-const fs = require("fs");
-const path = require("path");
-
+// EXPRESS SERVER DELIVERY 📫 //
 const app = express();
 
-const routes1 = require("./routes/index.js");
-const db = require("./db/db.json");
-const { ppid } = require("process");
+// SET PORT //
+const PORT = process.env.PORT || 3001;
 
-const PORT = process.env.PORT || 3000;
+// PARSE STRING / ARRAY 🍓 //
+app.use(express.urlencoded({ extended: true }));
 
 // PARSE JSON 🍓 //
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/", "index.js");
 
 // SET A STATIC FOLDER 🍓 //
-app.use(express.static(path.join(__dirname, "public")));
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
-// LOAD NOTES FROM DB 🍓 //
-app.get("/lib/notes", (req, res) => {
-    res.json(db);
+// ROUTERS USE WHAT 🍓 //
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+// LISTEN WHERE? + TIME 👀👂🕓 //
+app.listen(PORT, () => {
+    console.log(`Server LIVE on port: ${PORT}; ${moment().format("MMMM Do YYYY, h:mm:ss A")}!`);
 });
-
-// NOTE 🍓 //
-app.post("lib/notes", (req, res) => {
-    const newNote = req.body;
-    newNote.id = uuid();
-    const note = req.body;
-    db.push(note);
-    fs.writeFile("./db/db.json", JSON.stringify(db, null, 2), function (err, data) {
-        if (err) {
-            throw err
-        } else {
-            res.send(data)
-        }
-    });
-});
-
-// LOOK FOR NOTE BY ID 🍓 //
-function findNoteByID(id, db) {
-    const result = db.filter(note => note.id === id)[0];
-    return result;
-};
-
-// DELETE NOTE 🍓 //
-app.delete("//:id", (req, res) => { 
-    const result = findNoteByID(req.params.id, db);
-    console.log(result);
-    const index = (db.indexOf(result));
-    console.log(index);
-    db.splice(index, 1);
-    fs.writeFile("db/db.json", JSON.stringify(db , null, 2), function (err, data) {
-      if (err) {
-        throw err
-      } else {
-        res.send(data)
-      }
-    });
-  });
-
-// FIRST ROUTE 🍓 //
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-  });
-  
-// LISTEN WHERE? + TIME 🍓 //
-  app.listen(PORT, () => {
-    console.log(`Server now on port: ${PORT}; ${moment().format()}!`);
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// RETURN JSON //
-// app.get("db.json", (req,res) => {
-//     res.json(gettingsomevar);
-// });
-
-
-// app.get("/", (req,res) => {
-//     res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
