55fb24ec4f0f57ed1793c7333246c70e6519ba08
SJRXD <rohr.olasz@gmail.com>
2021-08-31 21:55:53 -0700

    adding notes to my routes again

--- Application/routes/htmlRoutes.js
+++ Application/routes/htmlRoutes.js
@@ -3,16 +3,19 @@ const path = require("path");
 const router = require("express").Router();
 
 // // GET NOTES PAGE //
+// request the notes page
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 // GET INDEX PAGE //
+// requests the index page
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // NO ROUTE / NO PROBLEM 🍒 //
+// if no route, redirect to index
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
