28ccb8a12af091fb80f32664a4045a544f205e87
SJRXD <rohr.olasz@gmail.com>
2021-08-31 21:51:48 -0700

    adding a few comments to saveData file

--- Application/lib/saveData.js
+++ Application/lib/saveData.js
@@ -8,13 +8,16 @@ const writeNote = util.promisify(fs.writeFile);
 
 class Save {
     write(note) {
+        // write method is passed note, note is written to json db
         return writeNote("db/db.json", JSON.stringify(note));
     }
 
     read() {
+        // read method returns a note from json db
         return readNote("db/db.json", "utf8");
     }
     async getNotes() {
+        // await read method, then get and add to parsedNotes array
         const notes = await this.read();
         let parsedNotes;
         try {
@@ -30,17 +33,20 @@ class Save {
             throw new Error("Not Title nor Text can be Blanketh.");
         }
         const newNote = { title, text, id: uuidv4() };
-        // this might not exactly be needed.
+        // this might not exactly be needed
+        // don't allow note to be posted if title or text is blank
 
         const notes = await this.getNotes();
         const updatedNotes = [...notes, newNote];
         await this.write(updatedNotes);
         return newNote;
+        // manages notes when they're updated
     }
     async deleteNote(id) {
         const notes = await this.getNotes();
         const filteredNotes = notes.filter(note => note.id !== id);
         return await this.write(filteredNotes);
+        // deletes a note by the note's id!
     }
 };
 
